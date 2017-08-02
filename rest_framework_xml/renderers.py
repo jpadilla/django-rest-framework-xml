5a62ed7f8f724ccb1669359a59c28844bc8ed3c0
Philipp Allgeuer <philipp.allgeuer@zumtobelgroup.com>
2017-08-02 09:06:25 +0200

    impl. new list item feature

--- rest_framework_xml/renderers.py
+++ rest_framework_xml/renderers.py
@@ -8,6 +8,7 @@ from django.utils.xmlutils import SimplerXMLGenerator
 from django.utils.six.moves import StringIO
 from django.utils.encoding import force_text
 from rest_framework.renderers import BaseRenderer
+from xml.etree import ElementTree as ET
 
 
 class XMLRenderer(BaseRenderer):
@@ -20,6 +21,7 @@ class XMLRenderer(BaseRenderer):
     charset = 'utf-8'
     item_tag_name = 'list-item'
     root_tag_name = 'root'
+    override_item_tag_name = False
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         """
@@ -38,7 +40,22 @@ class XMLRenderer(BaseRenderer):
 
         xml.endElement(self.root_tag_name)
         xml.endDocument()
+
+        if self.override_item_tag_name:
+            self._do_override_item_tag_name(stream)
+
         return stream.getvalue()
+
+    def _do_override_item_tag_name(self, stream):
+        root = ET.fromstring(stream.getvalue())
+        for parent in root.findall('.//*list-item/..'):
+            child_name = parent.tag[0:-1]
+            for child in parent.getchildren():
+                child.tag = child_name
+
+        stream.truncate(0)
+        stream.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        stream.write(ET.tostring(root))
 
     def _to_xml(self, xml, data):
         if isinstance(data, (list, tuple)):
